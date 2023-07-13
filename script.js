455ed973e6e8a4e166170f7fa4c4f5df1a1c1711
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-07-13 16:02:26 -0700

    starting to only intake letters

---
+++ script.js
@@ -0,0 +1,12 @@
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+document
+  .querySelector(".tester-input")
+  .addEventListener("keydown", function (event) {
+    // uses the isLetter function from above
+    if (!isLetter(event.key)) {
+      event.preventDefault();
+    }
+  });
