dab3f1b13e5777b8ae40b097ce353812c904b430
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-07-13 16:33:59 -0700

    added event listener to all letters

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
-document
-  .querySelector(".tester-input")
-  .addEventListener("keydown", function (event) {
-    // uses the isLetter function from above
+let textInput = document.querySelectorAll(".tester-input");
+
+for (let i = 0; i < textInput.length; i++) {
+  const element = textInput[i];
+  element.addEventListener("keydown", function (event) {
     if (!isLetter(event.key)) {
       event.preventDefault();
     }
   });
+}
