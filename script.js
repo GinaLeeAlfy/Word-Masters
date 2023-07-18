2fed1c599eb6b016a0afec0d1076e63b05685a30
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-07-18 14:08:02 -0700

    attempt at simulating tab

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ function isLetter(letter) {
 }
 
 let baseTextInput = document.querySelectorAll(".base-tester-input");
-let finalTextInput = document.querySelectorAll(".base-tester-input");
+let finalTextInput = document.querySelectorAll(".final-tester-input");
 
 for (let i = 0; i < baseTextInput.length; i++) {
   const element = baseTextInput[i];
-  element.addEventListener("keydown", function (event) {
-    if (event.key == "Backspace") {
-      return;
-    } else if (!isLetter(event.key)) {
-      event.preventDefault();
+  // element.addEventListener("keydown", function (event) {
+  //   if (event.key == "Backspace") {
+  //     return;
+  //   } else if (!isLetter(event.key)) {
+  //     event.preventDefault();
+  //   }
+  // });
+
+  element.addEventListener("keyup", function (event) {
+    if (isLetter(event.key)) {
+      function simulateKeyPress(key) {
+        const event = new KeyboardEvent("keyup", { key });
+        element.dispatchEvent(event);
+      }
+      simulateKeyPress("Tab");
     }
   });
 }
