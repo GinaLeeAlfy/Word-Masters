ac7bbd517fdb85a0e7404cb13bb38754bec9e935
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-07-18 14:17:03 -0700

    Not sure for tab

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ let finalTextInput = document.querySelectorAll(".final-tester-input");
 
 for (let i = 0; i < baseTextInput.length; i++) {
   const element = baseTextInput[i];
-  // element.addEventListener("keydown", function (event) {
-  //   if (event.key == "Backspace") {
-  //     return;
-  //   } else if (!isLetter(event.key)) {
-  //     event.preventDefault();
-  //   }
-  // });
-
-  element.addEventListener("keyup", function (event) {
-    if (isLetter(event.key)) {
-      function simulateKeyPress(key) {
-        const event = new KeyboardEvent("keyup", { key });
-        element.dispatchEvent(event);
-      }
-      simulateKeyPress("Tab");
+  element.addEventListener("keydown", function (event) {
+    if (event.key == "Backspace") {
+      return;
+    } else if (!isLetter(event.key)) {
+      event.preventDefault();
     }
   });
+
+  // element.addEventListener("keyup", function (event) {
+  //   if (isLetter(event.key)) {
+  //     function simulateKeyPress(key) {
+  //       const event = new KeyboardEvent("keyup", { key });
+  //       element.dispatchEvent(event);
+  //     }
+  //     simulateKeyPress("Tab");
+  //   }
+  // });
 }
 
 for (let i = 0; i < finalTextInput.length; i++) {
