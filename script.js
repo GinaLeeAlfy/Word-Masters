f03c31390f2c5dba5d375666ed06727dc5efdd8b
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-07-19 09:58:14 -0700

    uses focusNext to tab

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
+function focusNext() {
+  const currInput = document.activeElement;
+  const currInputIndex = inputs.indexOf(currInput);
+  const nextInputIndex = (currInputIndex + 1) % inputs.length;
+  const input = inputs[nextInputIndex];
+  input.focus();
+}
+
+let inputs = Array.prototype.slice.call(document.querySelectorAll("input"));
 let baseTextInput = document.querySelectorAll(".base-tester-input");
 let finalTextInput = document.querySelectorAll(".final-tester-input");
 
@@ -15,15 +24,11 @@ for (let i = 0; i < baseTextInput.length; i++) {
     }
   });
 
-  // element.addEventListener("keyup", function (event) {
-  //   if (isLetter(event.key)) {
-  //     function simulateKeyPress(key) {
-  //       const event = new KeyboardEvent("keyup", { key });
-  //       element.dispatchEvent(event);
-  //     }
-  //     simulateKeyPress("Tab");
-  //   }
-  // });
+  element.addEventListener("keyup", function (event) {
+    if (isLetter(event.key)) {
+      focusNext();
+    }
+  });
 }
 
 for (let i = 0; i < finalTextInput.length; i++) {
